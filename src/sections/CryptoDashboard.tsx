1cf60ca0f205e5842615432c2607bc7e30937f40
Ulises Molina <uli.molina10@gmail.com>
2025-01-21 01:22:25 -0300

    fix api bug

--- src/sections/CryptoDashboard.tsx
+++ src/sections/CryptoDashboard.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
 import Coin from "../types/CoinType";
 import Chart from "../components/Chart";
-import axios from "axios";
 
 export const CryptoDashboard = () => {
     const [coins, setCoins] = useState<Coin[]>([]);
     const [dolar, setDolar] = useState<number>(0);
     const [cryptoId, setCryptoId] = useState<string>("bitcoin");
     const [pesos, setPesos] = useState<number>(0);
+    const [error, setError] = useState<string | null>(null);
 
     const fetchCoins = async () => {
         const cachedData = sessionStorage.getItem("coins");
@@ -18,20 +18,12 @@ export const CryptoDashboard = () => {
         }
 
         try {
-            const response = await axios.get("/api/coins/markets", {
-                params: {
-                    vs_currency: "usd",
-                    order: "market_cap_desc",
-                    per_page: 16,
-                    page: 1,
-                    sparkline: false,
-                    price_change_percentage: "24h",
-                },
-            });
-            sessionStorage.setItem("coins", JSON.stringify(response.data));
-            setCoins(response.data);
+            const res = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            const data = await (await res).json();
+            setCoins(data);
+            sessionStorage.setItem("coins", JSON.stringify(data));
         } catch (error) {
-            console.error("Error fetching coins:", error);
+            setError("Error fetching coins:" + error);
         }
     };
 
@@ -62,7 +54,7 @@ export const CryptoDashboard = () => {
             <h2 className="hidden 2xl:flex absolute text-3xl font-bold text-white top-20 left-[220px]">Mercado de criptomonedas</h2>
             <div className="flex flex-col items-center gap-10 xl:flex-row">
             <div className="px-4 py-12 max-w-7xl sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 gap-4 py-24 2xl:grid-cols-2">
+                {error ? <p>Error fetching coins: {error}</p> : <div className="grid grid-cols-1 gap-4 py-24 2xl:grid-cols-2">
                     {coins.map((coin) => (
                         <Card
                             key={coin.id}
@@ -74,7 +66,7 @@ export const CryptoDashboard = () => {
                             setID={setCryptoId}
                         />
                     ))}
-                </div>
+                </div>}
             </div>
             <div className="relative w-1/2 p-5 mr-10 mb-8 border-b border-l rounded-lg min-w-[400px] md:min-w-[600px] border-white/20 self-start xl:mt-64">
                 <Chart cryptoId={cryptoId} />
