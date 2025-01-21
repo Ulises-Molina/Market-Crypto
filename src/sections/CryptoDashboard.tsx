342ba8c6e2440942c285f5382dd3c8c92b22e104
Ulises Molina <uli.molina10@gmail.com>
2025-01-21 01:28:03 -0300

    limit api response to 15 currencies

--- src/sections/CryptoDashboard.tsx
+++ src/sections/CryptoDashboard.tsx
@@ -18,7 +18,7 @@ export const CryptoDashboard = () => {
         }
 
         try {
-            const res = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            const res = fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=16&page=1&sparkline=false");
             const data = await (await res).json();
             setCoins(data);
             sessionStorage.setItem("coins", JSON.stringify(data));
